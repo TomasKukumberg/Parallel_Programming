982167803c2ef4cbe8381a4c1bbbb05a94917ccd
Tomáš Kukumberg <tomas.kukumberg@centrum.sk>
2022-04-25 20:23:15 +0200

    style: fix pep8 comment

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from fei.ppds import print
 
 @cuda.jit
 def my_kernel(data):
+    """Function that increments each element in the array by 50."""
     pos = cuda.grid(1)
     if pos < data.size:
         data[pos] += 50
@@ -26,4 +27,3 @@ BLOCKS_PER_GRID = (data.size + (THREADS_PER_BLOCK - 1)) // THREADS_PER_BLOCK
 
 my_kernel[BLOCKS_PER_GRID, THREADS_PER_BLOCK](data)
 print(data)
-
