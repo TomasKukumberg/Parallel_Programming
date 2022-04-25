8bd6f04707168e3b7f32e1fa2eacd64fd9569f46
Tomas <tomas@pop-os.localdomain>
2022-04-25 19:56:38 +0200

    feat: add first solution

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+Copyright 2022 Tomas Kukumberg <tomas.kukumberg@centrum.sk>
+This code is licensed under MIT license
+This module is dealing with Nvidia GPU computing problem.
+"""
+
+from numba import cuda
+import numpy
+from fei.ppds import print
+
+
+@cuda.jit
+def my_kernel(data):
+    pos = cuda.grid(1)
+    if pos < data.size:
+        data[pos] += 50
+
+
+SIZE = 122
+
+data = numpy.ones(SIZE)
+
+THREADS_PER_BLOCK = 32
+BLOCKS_PER_GRID = (data.size + (THREADS_PER_BLOCK - 1)) // THREADS_PER_BLOCK
+
+
+my_kernel[BLOCKS_PER_GRID, THREADS_PER_BLOCK](data)
+print(data)
+
